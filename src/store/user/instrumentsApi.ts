b79605ac6b82e423567f6fffffc43eab9986191a
Anton Antypenko <antipenko418@gmail.com>
2025-07-03 17:26:46 +0300

    fix url

--- src/store/user/instrumentsApi.ts
+++ src/store/user/instrumentsApi.ts
@@ -3,8 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const instrumentsApi = createApi({
   reducerPath: "instrumentsApi",
   baseQuery: fetchBaseQuery({
-    // baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/instruments`,
-    baseUrl: "http://localhost:3000/api/instruments",
+    baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/instruments`,
   }),
   tagTypes: ["Instruments"],
   endpoints: builder => ({
