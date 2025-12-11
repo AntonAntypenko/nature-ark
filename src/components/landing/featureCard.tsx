0d2bad3a93c5bddfa4fc88f503900d1b662accfc
Antypenko <antipenko418@gmail.com>
2025-12-11 04:53:09 +0200

    update featureCard with children props

--- src/components/landing/featureCard.tsx
+++ src/components/landing/featureCard.tsx
@@ -1,26 +1,30 @@
-import { ElementType, FC } from "react";
+import { ElementType, FC, ReactNode } from "react";
+
 import { Card, CardDescription, CardTitle } from "@/components/ui";
 
 type FeatureCardProps = {
-  icon: ElementType;
-  title: string;
-  description: string;
+  icon?: ElementType;
+  title?: string;
+  description?: string;
+  children?: ReactNode;
 };
 
 export const FeatureCard: FC<FeatureCardProps> = ({
   icon: Icon,
   title,
   description,
+  children,
 }: FeatureCardProps) => {
   return (
     <Card className="flex flex-col items-start p-6 text-left transition-shadow duration-300 hover:shadow-lg">
       <CardTitle className="flex gap-4 text-2xl font-semibold">
-        <Icon className="text-primary mb-4 h-8 w-8" />
+        {Icon && <Icon className="text-primary h-8 w-8 flex-shrink-0" />}
         {title}
       </CardTitle>
       <CardDescription className="text-muted-foreground text-lg">
         {description}
       </CardDescription>
+      <div>{children}</div>
     </Card>
   );
 };
