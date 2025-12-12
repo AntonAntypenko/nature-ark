b9337c941314db1a7ffc81a4e2e94ad272e5258c
Antypenko <antipenko418@gmail.com>
2025-12-12 04:10:47 +0200

    update finalCTASection.tsx

--- src/components/landing/finalCTASection.tsx
+++ src/components/landing/finalCTASection.tsx
@@ -7,14 +7,14 @@ export const FinalCTASection: FC = () => {
     <section className="w-full px-6 py-24">
       <div className="bg-primary-foreground mx-auto max-w-6xl rounded-xl p-12 text-center shadow-2xl">
         <h2 className="text-primary text-4xl font-bold">
-          Готові змінити облік у вашому зоопарку?
+          Почніть цифровий облік
         </h2>
         <p className="text-primary mt-4 text-xl">
           Почніть роботу з NatureArk вже сьогодні та отримайте повний контроль
           над витратами.
         </p>
 
-        <CtaButtonGroup className="mx-auto mt-8 space-x-6" />
+        <CtaButtonGroup className="mx-auto mt-8 space-x-2 md:mx-0 md:space-x-6" />
       </div>
     </section>
   );
