f10cb3f879663c8ca11f8e359b7802ca4c47cbec
Antypenko <antipenko418@gmail.com>
2026-01-21 15:46:32 +0200

    Update animation in stagger-item

--- src/components/motion/stagger-item.tsx
+++ src/components/motion/stagger-item.tsx
@@ -11,8 +11,9 @@ type StaggerItemProps = {
 };
 
 const itemVariants: Variants = {
-  hidden: { opacity: 0, y: 15 },
+  hidden: { opacity: 0, scale: 0.95, y: 10 },
   visible: {
+    scale: 1,
     opacity: 1,
     y: 0,
     transition: { duration: 0.4, ease: [0.25, 0.1, 0.25, 1] },
